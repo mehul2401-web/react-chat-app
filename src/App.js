62bf3299a06e6d82a893fe77d7f6a5a479712689
me-hulk <f20170401@hyderabad.bits-pilani.ac.in>
2023-01-17 01:10:51 +0530

    Added a login form to switch between users

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { ChatEngine } from "react-chat-engine";
+import LoginForm from "./components/LoginForm";
 import "./App.css";
 
 const projectID = "8fd74cab-4846-4ef3-a03e-0586ddfd9329";
 
 const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
       projectID={projectID}
-      userName={"me-hulk"}
-      userSecret={"1234"}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
     />
   );
 };
